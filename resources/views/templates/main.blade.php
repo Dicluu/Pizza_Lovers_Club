10ef0e933ef0ae4c605acb7953bbc350448ebb5c
Dicluu <alexsezz@list.ru>
2023-09-30 02:11:55 +0300

    remove excess hyperlinks from navbar and add a logo

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -21,7 +21,8 @@ use App\Models\PurchaseOrderTask;
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">PLC</a>
+        <img src="{{ asset('img/logo.png') }}" style="max-width: 24px; max-height: 24px">
+        <a class="navbar-brand" href=" {{ route('index') }}">PLC</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -29,7 +30,6 @@ use App\Models\PurchaseOrderTask;
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
                 </li>
                 @if(Auth::check())
                     @if(Auth::user()->can('viewAny', Item::class))
@@ -67,9 +67,6 @@ use App\Models\PurchaseOrderTask;
                         </li>
                     @endif
                 @endif
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
             </ul>
             <form class="d-flex" role="search">
                 @if(Auth::check())
